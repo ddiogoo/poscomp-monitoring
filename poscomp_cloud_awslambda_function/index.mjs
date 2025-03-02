fb21d0ab6287151c07ae6495027e6c824d321295
Diogo Martins de Assis <diogodmda@gmail.com>
2025-03-02 18:37:43 -0300

    feat: adapting lambda function to business rule about project

--- poscomp_cloud_awslambda_function/index.mjs
+++ poscomp_cloud_awslambda_function/index.mjs
@@ -7,9 +7,17 @@ export const handler = async (event) => {
         body: JSON.stringify({ message: "Missing 'message' field in request body." }),
       };
     }
+    const currentYear = new Date().getFullYear();
+    const currentPosComp = `POSCOMP ${currentYear}`;
+    let responseMessage;
+    if (body.message === currentPosComp) {
+      responseMessage = `Edital de inscrição para o ${currentPosComp} aberto! Se inscreva em: https://www.sbc.org.br/poscomp/`;
+    } else {
+      responseMessage = `Edital de inscrição para o ${currentPosComp} ainda NÃO aberto.`;
+    }
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: body.message }),
+      body: JSON.stringify({ message: responseMessage }),
     };
   } catch (error) {
     return {
